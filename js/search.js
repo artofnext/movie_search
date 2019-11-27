fdab4cdd53a5a1391ba1421e1efec343290dc881
artofnext <novnovalex@gmail.com>
2019-11-27 14:44:19 +0200

    add user input handler

--- js/search.js
+++ js/search.js
@@ -1,17 +1,33 @@
-//const API_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
-const API_URL = "https://old.bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
 
-fetch(API_URL)
-  .then(response => {
-    console.log("RESPONSE:", response);
+const API_URL = "http://www.omdbapi.com/?apikey=b336e268&t=trek";
+const API_URL_IMG = "http://img.omdbapi.com/?apikey=[yourkey]&";
+const API_KEY = "b336e268";
 
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    document.write("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
-  })
-  .catch(err => {
-    console.log("ERROR:", err.toString())
-  });
-  
+let searchField = document.getElementById('search');
+let typeField = document.getElementById('type');
+let searchButton = document.getElementById('search_button');
+searchButton.addEventListener('click', doRequest);
+
+function doRequest(event) {
+
+    let input = searchField.value.trim();
+    let type = typeField.value.trim();
+
+    console.log('Input: ' + input);
+    console.log('type: ' + type);
+
+    // fetch(API_URL)
+    //     .then(response => {
+    //         console.log("RESPONSE:", response);
+
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         console.log(data);
+    //         document.getElementById('debug').innerHTML = "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+    //         // document.write("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
+    //     })
+    //     .catch(err => {
+    //         console.log("ERROR:", err.toString())
+    //     });
+}
